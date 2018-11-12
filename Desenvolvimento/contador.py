c5e8e65addab455a2e7e05f06ca457445915bf4f
Victor Luiz <victor.smp13@gmail.com>
2018-11-12 14:42:07 -0200

    Implementada uma função para contar a quantidade de caracteres utilizáveis

--- Desenvolvimento/contador.py
+++ Desenvolvimento/contador.py
@@ -6,6 +6,7 @@ import string
 
 PONTUACOES_BRANCOS = string.punctuation
 PONTUACOES_BRANCOS += string.whitespace
+PONTUACOES_BRANCOS += string.digits
 ALFABETO = "qwertyuiopasdfghjklzxcvbnmáéíóúçüâêãõ"
 
 
@@ -34,6 +35,28 @@ def contador(filename):
                     count[letra.lower()] += 1
     count = sorted(count.items())
 
+    return count
+
+
+def ncaracters(filename):
+    """Função que conta a quantidade de caracteres utilizáveis
+
+    Parameters
+    ----------
+    filename : string
+        Nome do arquivo a ser analisado
+
+    Returns
+    -------
+    int
+        Quantidade de caracteres utilizáveis
+    """
+    count = 0
+    with open(filename, 'r') as arquivo:
+        for linha in arquivo:
+            for letra in linha:
+                if letra in ALFABETO:
+                    count += 1
     return count
 
 
